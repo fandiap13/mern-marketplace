5fdc2d7e0abb4b3b84ed53eb663b450a2bcb795e
Fandi Az <andiazizp123@gmail.com>
2024-01-11 20:34:29 +0700

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ mongoose
   .catch((err) => console.log(err));
 
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
