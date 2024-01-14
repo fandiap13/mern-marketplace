bff64df461bae77510a44ace90ef3c78eb1cabd7
Fandi Az <andiazizp123@gmail.com>
2024-01-14 21:42:46 +0700

    create redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+// ini digunakan ketika di refresh browsernya maka statenya tidak hilang
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: "root ",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  // Note: Codingan lama
+  //   reducer: {
+  //     user: userReducer,
+  //   },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
