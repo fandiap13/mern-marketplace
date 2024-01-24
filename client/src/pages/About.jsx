dff49c1a98da12ab918c75349729726e9f2ac4d9
Fandi Az <andiazizp123@gmail.com>
2024-01-24 16:04:45 +0700

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Fandi's Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis unde
+        labore quae fugiat inventore laboriosam ipsa suscipit culpa adipisci
+        distinctio. Laborum voluptate, deleniti exercitationem ipsum sunt error,
+        repellendus dignissimos dolore fugiat quibusdam vitae unde? Nobis
+        architecto cumque voluptate laborum provident assumenda, pariatur
+        repellat cupiditate sint nemo sit vitae magnam corrupti numquam
+        praesentium incidunt, excepturi totam neque natus! Ipsam, fuga possimus
+        totam dicta, obcaecati repellat doloremque modi necessitatibus eaque
+        voluptates dignissimos officia? Sapiente, blanditiis. Itaque aspernatur
+        cupiditate dolore, hic numquam iure dolorem neque quos facilis eligendi
+        illo voluptatum at distinctio fugit tempore fugiat consequatur quaerat
+        pariatur laboriosam minus. In, eligendi a.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis unde
+        labore quae fugiat inventore laboriosam ipsa suscipit culpa adipisci
+        distinctio. Laborum voluptate, deleniti exercitationem ipsum sunt error,
+        repellendus dignissimos dolore fugiat quibusdam vitae unde? Nobis
+        architecto cumque voluptate laborum provident assumenda, pariatur
+        repellat cupiditate sint nemo sit vitae magnam corrupti numquam
+        praesentium incidunt, excepturi totam neque natus! Ipsam, fuga possimus
+        totam dicta, obcaecati repellat doloremque modi necessitatibus eaque
+        voluptates dignissimos officia? Sapiente, blanditiis. Itaque aspernatur
+        cupiditate dolore, hic numquam iure dolorem neque quos facilis eligendi
+        illo voluptatum at distinctio fugit tempore fugiat consequatur quaerat
+        pariatur laboriosam minus. In, eligendi a.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis unde
+        labore quae fugiat inventore laboriosam ipsa suscipit culpa adipisci
+        distinctio. Laborum voluptate, deleniti exercitationem ipsum sunt error,
+        repellendus dignissimos dolore fugiat quibusdam vitae unde? Nobis
+        architecto cumque voluptate laborum provident assumenda, pariatur
+        repellat cupiditate sint nemo sit vitae magnam corrupti numquam
+        praesentium incidunt, excepturi totam neque natus! Ipsam, fuga possimus
+        totam dicta, obcaecati repellat doloremque modi necessitatibus eaque
+        voluptates dignissimos officia? Sapiente, blanditiis. Itaque aspernatur
+        cupiditate dolore, hic numquam iure dolorem neque quos facilis eligendi
+        illo voluptatum at distinctio fugit tempore fugiat consequatur quaerat
+        pariatur laboriosam minus. In, eligendi a.
+      </p>
+    </div>
+  );
 }
